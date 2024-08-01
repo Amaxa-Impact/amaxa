50ce1487416ac092dc0654515dd2b7e92329458b
xiften <108333567+not-ani@users.noreply.github.com>
2024-08-01 15:45:18 -0600

    fix: eslint error

--- apps/nextjs/src/app/(home)/action-guides/_components/GuideCard.tsx
+++ apps/nextjs/src/app/(home)/action-guides/_components/GuideCard.tsx
@@ -1,6 +1,7 @@
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 // @ts-nocheck
 
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
