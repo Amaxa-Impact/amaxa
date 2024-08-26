38cfdf546038d6b8c35c7ffb4772ceb73701f605
xiften <108333567+not-ani@users.noreply.github.com>
2024-08-26 10:46:53 -0600

    fix: all people displayed on leadership page

--- apps/website/src/app/(marketing)/team/_components/author-card.tsx
+++ apps/website/src/app/(marketing)/team/_components/author-card.tsx
@@ -8,28 +8,24 @@ interface MemberCardProps {
 
 export const PersonCard = ({ name, role, image }: MemberCardProps) => {
   return (
-    <div className="flex flex-col rounded-sm border bg-white md:flex-row">
-      <div className="group relative flex min-h-[400px] w-full flex-col py-10 dark:border-neutral-800 md:w-1/4 lg:border-b lg:border-l lg:border-r ">
-        <div className="group pointer-events-none absolute inset-0 h-full w-full bg-gradient-to-t from-neutral-100 to-transparent opacity-0 transition duration-200 group-hover:opacity-100 dark:from-neutral-800"></div>
-        <div className="w-full">
-          <Image
-            src={image}
-            fill={true}
-            className="w-full"
-            alt="Illustration 1"
-          />
-        </div>
+    <div className="group relative flex min-h-[400px] max-w-[350px] flex-col py-10 dark:border-neutral-800 lg:border-b lg:border-l lg:border-r">
+      <div className="group pointer-events-none absolute inset-0 h-full w-full bg-gradient-to-t from-neutral-100 to-transparent opacity-0 transition duration-200 group-hover:opacity-100 dark:from-neutral-800"></div>
+      <div className="w-full">
+        <Image
+          src={image}
+          priority
+          fill={true}
+          className="w-full"
+          alt="Illustration 1"
+        />
+      </div>
 
-        <span
-          className="absolute bottom-0 left-0 p-4 font-bold text-white"
-          style={{ textShadow: "0 4px 8px rgba(0, 0, 0, 1)" }}
-        >
-          {name} • {role}
-        </span>
-      </div>
-      <div className="items-start justify-center py-4 pl-10">
-        <h1 className="text-2xl font-bold">{name}</h1>
-      </div>
+      <span
+        className="absolute bottom-0 left-0 p-4 font-bold text-white"
+        style={{ textShadow: "0 4px 8px rgba(0, 0, 0, 1)" }}
+      >
+        {name} • {role}
+      </span>
     </div>
   );
 };
--- apps/website/src/app/(marketing)/team/page.tsx
+++ apps/website/src/app/(marketing)/team/page.tsx
@@ -10,16 +10,34 @@ const members = [
     role: "Founder & CEO",
   },
   {
-    name: "Lauren McMillen",
-    title: "Founder & CEO",
-    image: "/lauren-headshot.png",
-    role: "Founder & CEO",
+    name: "Lexi Jones",
+    title: "Head of Programs and Marketing",
+    image: "/lex-headshot.jpg",
+    role: "Head of Programs and Marketing ",
   },
   {
-    name: "Lauren McMillen",
-    title: "Founder & CEO",
-    image: "/lauren-headshot.png",
-    role: "Founder & CEO",
+    name: "Hasibur",
+    title: "Director of Student Experience ",
+    image: "/hasibur-headshot.png",
+    role: "Director of Student Experience ",
+  },
+  {
+    name: "Pari Singh",
+    title: "Director of Operations",
+    image: "/pari-headshot.png",
+    role: "Director of Operations",
+  },
+  {
+    name: "Heather Miller",
+    title: "Director of Digital Marketing",
+    image: "/heather-headshot.png",
+    role: "Director of Digital Marketing",
+  },
+  {
+    name: "Phoebe Cox",
+    title: "Head of Research and Strategy",
+    image: "/phoebe-headshot.png",
+    role: "Head of Research and Strategy",
   },
 ];
 
@@ -27,7 +45,7 @@ export default function Page() {
   return (
     <div>
       <h1 className="text-5xl font-bold">Meet The Team!</h1>
-      <div className="relative z-10 grid grid-cols-1  gap-2 py-10 ">
+      <div className="relative z-10 grid grid-cols-3 gap-10 py-10 ">
         {members.map((member, index) => (
           <PersonCard key={index} {...member} />
         ))}
