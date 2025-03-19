c86ffa162ac20172cd504d6225504351ec280e49
Laurenmcm749 <lm3623@nyu.edu>
2025-03-19 08:47:16 +0000

    deleted unused imports

--- apps/website/src/app/(marketing)/project/page.tsx
+++ apps/website/src/app/(marketing)/project/page.tsx
@@ -6,7 +6,9 @@ import { motion } from "framer-motion";
 
 import { BlurImage } from "~/components/ui/apple-cards";
 import { projects } from "./projects";
-import { ApplyButton } from "~/components/apply";
+
+
+
 
 const ProjectCard = ({
   project,
@@ -124,53 +126,16 @@ export default function Page() {
           </div>
      
       </section>
-
-        </div>
+       </div>
 
         <div className="flex h-screen flex-col gap-4 py-16">
           {
             projects.map((project, index) => <ProjectCard project={project} key={index} />)
           }
-        </div>
+        </div>  
+
       </section> 
 
-{/* <section className="w-full py-16 md:py-24 lg:py-32 px-6 md:px-16 lg:px-20">
-        <div className="max-w-7xl mx-auto">
-      
-       
-          <div className="space-y-16 md:space-y-24">
-         
-              <div>
-                <h3 className="text-2xl md:text-3xl font-normal text-[#3B3B3B] mb-4">
-                  Our projects and initiatives are high-impact.
-                </h3>
-                <p className="text-lg mb-6 max-w-3xl">
-                  Our projects are high-impact, innovative, and
-                  community-driven. We have partnered with nonprofits in
-                  Palestine, Liberia, and Uganda whose community-founded
-                  solutions are innovative in their fields, such as ISNAD
-                  Community Center's approach to community-based learning, the
-                  first in its area, and the Nyaka School's holistic approach to
-                  supporting AIDS-affected communities.
-                </p>
-                <p className="text-lg mb-6 max-w-3xl">
-                  Where we spotted gaps in high-impact projects that our members
-                  wanted to work on, we created our own. Led by Head of Programs
-                  Alexi Jones, we have launched three new initiatives in 2024,
-                  focusing on mental health, feminism, and LBGTQ+
-                  representation.
-                </p>
-                <a
-                  href="/projects"
-                  className="inline-flex items-center justify-center px-6 py-2 bg-[#BCD96C] rounded-full text-[#3B3B3B] hover:bg-[#a9c55a] transition-colors"
-                >
-                  Explore All Projects →
-                </a>
-              </div>
-            </div>
-          </div>
-     
-      </section> */}
 
       
 
