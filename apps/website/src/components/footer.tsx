0ef419637cb12cffb7351a3aeb40d6124dec5f4a
Laurenmcm749 <lm3623@nyu.edu>
2025-03-30 13:30:23 +0100

    fix: background image and updated styling for newsletter section

--- apps/website/src/components/footer.tsx
+++ apps/website/src/components/footer.tsx
@@ -1,32 +1,44 @@
 import React from "react";
 import Link from "next/link"; // Assuming you're using Next.js, otherwise use React Router
+import Image from "next/image";
 
 const Footer = () => {
   return (
-    <footer className="w-full bg-white border-t border-gray-200">
-         <div className="flex flex-col relative bg-[#b9d66e] py-5">
-      {/* Subscribe section */}
-      <div className="flex flex-col p-10 rounded-[25px] h-[64.51%] ">
-        <div className="font-normal text-[16px] leading-[24px] text-[#3B3B3B] mb-8">
-          STAY IN THE LOOP
-        </div>
-        <h2 className="font-normal text-[48px] leading-[60px] text-[#3B3B3B] mb-8 max-w-[1031px]">
+    <footer className="w-full h-[450px] bg-black border-t border-gray-200 ">
+
+    <section 
+      className="w-full h-full py-16 md:py-244 bg-cover bg-center"
+      style={{
+        backgroundImage:
+          'url("https://b47pkz22xs.ufs.sh/f/OxFTTzjZGToOf96x5gJwr1ELXMUHWOkNjK5T3SJYnup9ReGz")',
+          
+      }}>
+
+   <div className="flex flex-col relative">
+      <div className="flex flex-col p-10 rounded-[25px]">
+        <h2 className="font-normal text-[48px] leading-[60px] text-[#3B3B3B] mb-8 max-w-[1031px] bg-white p-5  ">
           Check out our{" "}
           <span className="font-semibold">highly reviewed</span> newsletter.
         </h2>
-        {/* <div className="flex flex-row items-center justify-between max-w-[1031px]">
-          <p className="font-normal text-[32px] leading-[40px] text-[#3B3B3B]">
-            We promise it's worth your while.
-          </p>
-        </div> */}
         <div className="flex flex-row items-center justify-between max-w-[1031px]">
-        <Link href="https://amaxaimpact.substack.com/subscribe?utm_source=email&utm_campaign=email-subscribe&r=1qhobc&next=https%3A%2F%2Famaxaimpact.substack.com%2Fp%2Fmarch-preview&utm_medium=email"   className="font-normal text-[24px] md:text-[24px] lg:text-[24px] leading-tight text-[#3B3B3B] hover:opacity-80 transition-opacity underline">
+        <h2 className="font-normal text-[40px] leading-[60px] text-[#3B3B3B]  max-w-[1031px] bg-white underline p-5 mb-8">
+        <Link 
+        href="https://amaxaimpact.substack.com/subscribe?utm_source=email&utm_campaign=email-subscribe&r=1qhobc&next=https%3A%2F%2Famaxaimpact.substack.com%2Fp%2Fmarch-preview&utm_medium=email"   
+       >
         Subscribe Now → 
         </Link>
-      </div>    
+        </h2>
+        </div>
+        
+        </div>
+
     </div>
-      {/* Footer text section */}
-    </div>
+
+  </section>
+
+
+      {/* NORMAL FOOTER */}
+
       <div className="container py-8 mx-auto flex flex-col md:flex-row justify-between items-center">
         <div className="mb-4 md:mb-0">
           <Link href="/">
@@ -92,6 +104,10 @@ const Footer = () => {
       {/* <div className="container mx-auto mt-4 text-sm text-gray-600">
         <p>Ámaxa is a registered 501(c)(3) nonprofit.</p>
       </div> */}
+
+
+
+
     </footer>
   );
 };
