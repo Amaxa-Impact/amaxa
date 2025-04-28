c0d2f68f2840ee661e2a124e2f3cc46af42c69e3
Laurenmcm749 <lm3623@nyu.edu>
2025-04-28 11:26:48 +0100

    fix: reversed previous commit, uncommented permissions.ts

--- packages/api/src/permissions.ts
+++ packages/api/src/permissions.ts
@@ -1,49 +1,49 @@
 
-// import type { Session } from "@amaxa/auth";
+import type { Session } from "@amaxa/auth";
 
-// export function isProjectStudent(projectId: string, session: Session | null) {
-//   if (!session) return false;
-//   if (session.user.role === "Admin") return true;
-//   const projectPermissions = session.user.project_permissions;
-//   if (!projectPermissions) return false;
-//   return (
-//     projectPermissions[projectId] === "admin" ||
-//     projectPermissions[projectId] === "coach" ||
-//     projectPermissions[projectId] === "student"
-//   );
-// }
+export function isProjectStudent(projectId: string, session: Session | null) {
+  if (!session) return false;
+  if (session.user.role === "Admin") return true;
+  const projectPermissions = session.user.project_permissions;
+  if (!projectPermissions) return false;
+  return (
+    projectPermissions[projectId] === "admin" ||
+    projectPermissions[projectId] === "coach" ||
+    projectPermissions[projectId] === "student"
+  );
+}
 
-// /*
-//   param projectId: string
-//   @param session: Session | null
-//   @returns boolean
+/*
+  param projectId: string
+  @param session: Session | null
+  @returns boolean
 
-//   Returns true if the user has admin or coach privileges for the project
-// */
-// export function isProjectPrivileged(
-//   projectId: string,
-//   session: Session | null,
-// ) {
-//   if (!session) return false;
-//   if (session.user.role === "Admin") return true;
-//   const projectPermissions = session.user.project_permissions;
-//   if (!projectPermissions) return false;
-//   return (
-//     projectPermissions[projectId] === "admin" ||
-//     projectPermissions[projectId] === "coach"
-//   );
-// }
+  Returns true if the user has admin or coach privileges for the project
+*/
+export function isProjectPrivileged(
+  projectId: string,
+  session: Session | null,
+) {
+  if (!session) return false;
+  if (session.user.role === "Admin") return true;
+  const projectPermissions = session.user.project_permissions;
+  if (!projectPermissions) return false;
+  return (
+    projectPermissions[projectId] === "admin" ||
+    projectPermissions[projectId] === "coach"
+  );
+}
 
-// export function isProjectAdmin(projectId: string, session: Session | null) {
-//   if (!session) return false;
-//   if (session.user.role === "Admin") return true;
-//   const projectPermissions = session.user.project_permissions;
-//   if (!projectPermissions) return false;
-//   return projectPermissions[projectId] === "admin";
-// }
+export function isProjectAdmin(projectId: string, session: Session | null) {
+  if (!session) return false;
+  if (session.user.role === "Admin") return true;
+  const projectPermissions = session.user.project_permissions;
+  if (!projectPermissions) return false;
+  return projectPermissions[projectId] === "admin";
+}
 
-// export function isAdmin(session: Session | null) {
-//   if (!session) return false;
-//   if (session.user.role === "Admin") return true;
-//   return false;
-// }
+export function isAdmin(session: Session | null) {
+  if (!session) return false;
+  if (session.user.role === "Admin") return true;
+  return false;
+}
