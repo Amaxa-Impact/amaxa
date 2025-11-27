2c44593e07207839e9d7a97cc0cb7f23840b0597
Ani <108333567+not-ani@users.noreply.github.com>
2025-11-26 21:58:21 -0700

    refactor: removed all old deps

--- apps/nextjs/src/app/api/auth/[...all]/route.ts
+++
@@ -1,5 +0,0 @@
-import { auth } from "@amaxa/auth";
-
-const handler = auth.handler;
-
-export { handler as GET, handler as POST };
--- packages/auth/src/client.ts
+++
@@ -1,5 +0,0 @@
-import { createAuthClient } from "better-auth/react";
-
-export const authClient = createAuthClient();
-
-export const { signIn, signOut, signUp, useSession } = authClient;
